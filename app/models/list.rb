261a1dff363647e9bf751520061c116025e02aec
Benson Lim <pugnusferreus@192-168-1-6.tpgi.com.au>
2010-11-23 23:13:28 +1100

    added has many sub categories

--- app/models/list.rb
+++ app/models/list.rb
@@ -9,4 +9,35 @@ class List
     @sub_category_id = sub_category_id
   end
   
+  def to_data_table(cart)
+    
+    data_table = {"sEcho" => 1}
+    data_table["iTotalRecords"] = @item.count
+    data_table["iTotalDisplayRecords"] = @item.count
+    
+    item_array = Array.new
+    
+    @item.each do |item|
+      row = Array.new(6)
+      row[0] = item.name
+      row[1] = item.author
+      row[2] = item.status_description
+      row[3] = item.description
+      row[4] = item.sub_category.category.name
+      if not item.loaned?
+        if not cart.nil? and cart[item.id]
+          row[5] = "<a href='/cart/#{item.id}/remove'>Cancel</a>"
+        else
+          row[5] = "<a href='/cart/#{item.id}'>Put In Cart</a>"
+        end
+      else
+        row[5] = "Loaned"
+      end
+      item_array.push row
+    end
+    data_table["aaData"] = item_array
+    
+    return data_table
+  end
+  
 end
