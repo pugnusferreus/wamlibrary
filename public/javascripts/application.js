078d7aebdb8bd63132de79d8e8fc894d1bd916c3
Benson Lim <pugnusferreus@192-168-1-6.tpgi.com.au>
2010-12-01 21:10:33 +1100

    added navigation on all page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  $('.main_menu').hide();
+  $('#[id^=navigation_link_]').click(function(){
+    $('#' + this.id.replace("link_","")).toggle();
+  });
+});
