faf0d0cc404e002a84c0b9b1abf3ab0d32166a36
Benson Lim <pugnusferreus@Benson-Lims-MacBook-Pro-15.local>
2010-12-13 23:10:09 +1100

    jquery corner doesn't work well with IE. totally no border

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   $('#[id^=navigation_link_]').click(function(){
     $('#' + this.id.replace("link_","")).toggle();
   });
-
+/*
   $('#header_content').corner();
   $('.text_box_wrapper').corner();
   $('.text_area_wrapper').corner();
-
+*/
 
 });
