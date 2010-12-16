bc664253571965b980828339d8bf6827d9bb123d
Benson Lim <pugnusferreus@Benson-Lims-MacBook-Pro-15.local>
2010-12-17 07:53:16 +1100

    added no caching I've noticed that IE loves to cache everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :prevent_caching
   protect_from_forgery
+  
+  def prevent_caching
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
   
 protected
   def is_admin
