b516ec9fa7cd2fd485da108f9593c48a4409fe50
Benson Lim <pugnusferreus@192-168-1-6.tpgi.com.au>
2011-01-27 23:19:26 +1100

    due_date should be set to nil as well

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -95,7 +95,7 @@ class ItemsController < ApplicationController
   def return
     item = Item.find(params[:id])
     Log.create("requester" => item.loaned_by, "item" => item.id, "log_type" => Log::RETURN)
-    Item.update(item.id, {:loaned => false, :loaned_by => nil, :loaned_date => nil, :updated_at => Time.now })
+    Item.update(item.id, {:loaned => false, :loaned_by => nil, :loaned_date => nil, :updated_at => Time.now, :due_date => nil })
     respond_to do |format|
       format.html { redirect_to items_path }
     end
